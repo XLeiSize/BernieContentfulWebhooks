558d243e5a2b779803cd3d1343681333cae250a3
Thomas Boulongne <thomasboulongne@hotmail.com>
2017-06-04 23:35:25 +0200

    Add log of request

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ var server = require('contentful-webhook-server')();
 // Is not emitted when an error occurs
 server.on('ContentManagement.*', function(topic, req){
 
-  // topic is available as string
-  // => e.g. ContentManagement.Asset.unpublish
-  console.log('Request came in for: ' + topic);
+	// topic is available as string
+	// => e.g. ContentManagement.Asset.unpublish
+	console.log('Request came in for: ' + topic);
+	console.log('Data retrieved: ' + req);
 });
 
 // Start listening for requests on port 3000
 const port = process.env.PORT || 8080;
 server.listen(port, function(){
-  console.log('Contentful webhook server running on port ' + port)
+	console.log('Contentful webhook server running on port ' + port)
 });
