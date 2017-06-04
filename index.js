53576a5509bcc54f12556dc444ab80fdcafa1c05
Thomas Boulongne <thomasboulongne@hotmail.com>
2017-06-05 00:34:10 +0200

    Switched back to contentful-webhook-listener

--- index.js
+++ index.js
@@ -1,42 +1,13 @@
-// Create webhook server
-var server = require('contentful-webhook-server')();
+var listener = require("contentful-webhook-listener");
+var webhook = listener.createServer();
+const port = process.env.PORT || 8080;
 
-const util = require('util')
+webhook.on("ContentManagement.Entry.publish", function (payload) {
 
-// Handler for all successful requests
-// Is not emitted when an error occurs
-server.on('ContentManagement.Entry.publish', function(req){
+	console.log(payload);
 
-	let body = "";
-
-	req.on("data", function (chunk) {
-
-		body += chunk.toString();
-
-	});
-
-	// parse body
-	req.on("end", function () {
-		try {
-		  body = JSON.parse(body);
-			console.log(body);
-		} catch (err) {
-		}
-	});
-
-	if(req.contentType == 'artwork') {
-		const artwork = req.fields;
-		if(artwork.images) {
-			for (var i = 0; i < artwork.images.length; i++) {
-				const image = artwork.images[i].fields.file
-				console.log(image);
-			}
-		}
-	}
 });
 
-// Start listening for requests on port 3000
-const port = process.env.PORT || 8080;
-server.listen(port, function(){
+webhook.listen(port, function(){
 	console.log('Contentful webhook server running on port ' + port)
 });
