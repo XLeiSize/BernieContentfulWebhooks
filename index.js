e3bc239e6d84f15693c759e20c74eea9bfcce4a3
Thomas Boulongne <thomasboulongne@hotmail.com>
2017-06-05 12:52:10 +0200

    Add content-type condition

--- index.js
+++ index.js
@@ -19,46 +19,46 @@ const port = process.env.PORT || 8080;
 webhook.on("publish", function (payload) {
 
 	console.log(util.inspect(payload, false, null));
+	if(payload.contentType == 'artwork') {
+		const obj = payload.fields;
 
-	const obj = payload.fields;
+		if(obj.images['fr-FR']) {
+			obj.images['fr-FR'].forEach(e => {
+				const id = e.sys.id;
 
-	if(obj.images['fr-FR']) {
-		obj.images['fr-FR'].forEach(e => {
-			const id = e.sys.id;
+				console.log("Image id", id);
 
-			console.log("Image id", id);
-
-			Contentful.getAsset(id)
-			.then( response => {
-				const uniqId = obj.slug + '_' + id;
-				console.log(uniqId);
-				// PUT VUFORIA CALL HERE
-			})
-			.catch( err => {
-				console.log(err);
+				Contentful.getAsset(id)
+				.then( response => {
+					const uniqId = obj.slug + '_' + id;
+					console.log(uniqId);
+					// PUT VUFORIA CALL HERE
+				})
+				.catch( err => {
+					console.log(err);
+				});
 			});
+		}
+
+		const ApiaiConfig = {
+
+			url: Apiai.url + 'entities/' + Apiai.artworkEntityId + '/entries' + Apiai.version,
+
+			method: 'post',
+
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': 'Bearer ' + Apiai.devToken
+			},
+
+			data: '[{"value": "' + obj.title + '"}]'
+		};
+
+		Axios(ApiaiConfig)
+		.then(resp => {
+			console.log(response);
 		});
 	}
-
-	const ApiaiConfig = {
-
-		url: Apiai.url + 'entities/' + Apiai.artworkEntityId + '/entries' + Apiai.version,
-
-		method: 'post',
-
-		headers: {
-			'Content-Type': 'application/json',
-			'Authorization': 'Bearer ' + Apiai.devToken
-		},
-
-		data: '[{"value": "' + obj.title + '"}]'
-	};
-
-	Axios(ApiaiConfig)
-	.then(resp => {
-		console.log(response);
-	});
-
 });
 
 webhook.listen(port, function(){
