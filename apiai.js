0786899f7b8ff68b42f1f9b2cdae91852c31d83a
Thomas Boulongne <thomasboulongne@hotmail.com>
2017-06-05 13:00:56 +0200

    Fix api ai entity id

--- apiai.js
+++ apiai.js
@@ -2,5 +2,5 @@ module.exports = {
 	devToken: 'c8a66405a4dd4b01aa5252c6f16fcd03',
 	url: 'https://api.api.ai/v1/',
 	version: '?v=20150910',
-	artworkEntityId: 'b0da513e-69f7-4bea-bf26-03b38fc8019b/editEntity/106fc9ca-b2df-4f12-bf76-44c1a6228869'
+	artworkEntityId: '106fc9ca-b2df-4f12-bf76-44c1a6228869'
 };
