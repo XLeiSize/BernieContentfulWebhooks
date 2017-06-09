20c3f469e04a7897f36214291b2f7cb38adbed64
Lei Xing <lei.xing@akqa.com>
2017-06-09 20:26:37 +0200

    change token

--- apiai.js
+++ apiai.js
@@ -2,5 +2,5 @@ module.exports = {
 	devToken: '8035d9c5ed1d4e50a4f95fec8e2fb896',
 	url: 'https://api.api.ai/v1/',
 	version: '?v=20150910',
-	artworkEntityId: 'a2d431d1-bcf8-4f02-a370-1719851514c3'
+	artworkEntityId: '3215d57c-f740-4089-b173-1d516629810d'
 };
