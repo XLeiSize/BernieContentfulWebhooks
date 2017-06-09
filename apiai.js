e910d6e662df3011ad48880934e84261e4b096a2
Lei Xing <lei.xing@akqa.com>
2017-06-09 20:13:03 +0200

    update token

--- apiai.js
+++ apiai.js
@@ -1,6 +1,6 @@
 module.exports = {
-	devToken: 'c8a66405a4dd4b01aa5252c6f16fcd03',
+	devToken: '8035d9c5ed1d4e50a4f95fec8e2fb896',
 	url: 'https://api.api.ai/v1/',
 	version: '?v=20150910',
-	artworkEntityId: '106fc9ca-b2df-4f12-bf76-44c1a6228869'
+	artworkEntityId: 'a2d431d1-bcf8-4f02-a370-1719851514c3'
 };
